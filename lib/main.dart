fe398e60070449bcfb41f12f6df7432f0ba0c73e
IsraaDrafli <israadrafli55@gmail.com>
2026-03-26 16:31:15 +0100

    N

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,16 @@ import 'package:car_app/signInANDsignUp/signup.dart';
 import 'package:car_app/signInANDsignUp/startingpage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
